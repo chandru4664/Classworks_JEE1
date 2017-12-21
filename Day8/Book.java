d9ab5adb10fe49276d369aa7f7004ca48fe10656
HTC Training <31919061+HTCTraining@users.noreply.github.com>
2017-12-21 08:58:46 +0530

    Add files via upload

---
+++ Day8/Book.java
@@ -0,0 +1,70 @@
+package com.htc.arraylistsample;
+// Book Bean Class / Book POJO class / Book Entity class 
+public class Book {
+	  
+		private int id;  
+		private String name,author,publisher;  
+		private int quantity;  
+		public Book(int id, String name, String author, String publisher, int quantity) {  
+		    this.id = id;  
+		    this.name = name;  
+		    this.author = author;  
+		    this.publisher = publisher;  
+		    this.quantity = quantity;  
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getAuthor() {
+			return author;
+		}
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		public String getPublisher() {
+			return publisher;
+		}
+		public void setPublisher(String publisher) {
+			this.publisher = publisher;
+		}
+		public int getQuantity() {
+			return quantity;
+		}
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
+		@Override
+		public String toString() {
+			return "Book [id=" + id + ", name=" + name + ", author=" + author + ", publisher=" + publisher
+					+ ", quantity=" + quantity + "]";
+		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + id;
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Book other = (Book) obj;
+			if (id != other.id)
+				return false;
+			return true;
+		}  
+}
